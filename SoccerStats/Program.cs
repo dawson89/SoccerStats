7705cc84329af50fddebe56075868e44000f6df1
Dawson Richey <dawsonrichey@gmail.com>
2018-10-07 18:49:43 -0400

    Applied Split effect to the data output

--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -17,9 +17,12 @@ namespace SoccerStats
 			DirectoryInfo directory = new DirectoryInfo(currentDirectory);
 			var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
 			var fileContents = ReadFile(fileName);
-			Console.WriteLine(fileContents);
-
-
+			string[] fileLines = fileContents.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+			foreach(var line in fileLines)
+			{
+				Console.WriteLine(line);
+			}
+				
 		}
 
 		public static string ReadFile(string filename)
