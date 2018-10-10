c48685ee8f65993a5716dcd0a36e92e1e28efd87
Dawson Richey <dawsonrichey@gmail.com>
2018-10-09 20:48:45 -0400

    Assigned a calculation to a new object

--- SoccerStats/GameResult.cs
+++ SoccerStats/GameResult.cs
@@ -16,6 +16,14 @@ namespace SoccerStats
 		public int ShotsOnGoal{ get; set; }
 		public int ShotsOffGoal { get; set; }
 		public double PosessionPercent { get; set; }
+		public double ConversionRate
+		{
+			get
+			{
+				return (double)Goals / (double)GoalAttempts;
+			}
+		}
+		
 	}
 		public enum HomeOrAway
 		{
